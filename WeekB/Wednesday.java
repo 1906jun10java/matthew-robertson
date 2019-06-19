5861905306b24a4a65d3973a662dd6b98ff6dee5
Matthew Robertson <matt.robertson904@gmail.com>
2019-06-19 16:56:34 -0400

    Reverse

--- WeekB/Wednesday.java
+++ WeekB/Wednesday.java
@@ -3,7 +3,9 @@ public class Wednesday {
 
 	public static void main(String [] args) {
 		 
-		reverse("Stanely Yelnats");
+		 reverse("StanelyYelnats");
+		// calculateFactorial(7);
+		// isPalindrome("StanleyYelnats");
 	}
 	
 		public static void reverse(String term) {
@@ -13,4 +15,25 @@ public class Wednesday {
 			}
 		}
 	
-}
+		public static void isPalindrome(String term) {
+			char[] ys = term.toCharArray();		
+			for (int i = ys.length-1; i >= 0; i--) {
+					char reverse = ys[i];
+				}
+					if(reverse == ys)
+				}
+		}
+}		
+		/*
+		public static int calculateFactorial(int n) {
+			// base case
+			if(n == 0 || n == 1) {
+				return 1;
+				
+			}else
+			// recursion case
+				return n * calculateFactorial(n-1);
+		}
+}*/
+
+
