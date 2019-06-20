2e09447a8ebad6b5ad90668e4407d9d08732c1e3
Matthew Robertson <matt.robertson904@gmail.com>
2019-06-20 16:59:05 -0400

    Thursday

---
+++ WeekB/Thursday.java
@@ -0,0 +1,22 @@
+
+public class Thursday {
+
+	public static void main(String [] args) {
+		
+		containsSubString("Hello World!","ello");
+		
+	}
+	
+	public boolean containsSubString(String str, String substr) {
+		
+		char [] rts = str.toCharArray(); 
+			for (int i = 0; i <= rts.length; i++) {
+				for(j = 0; j >= rts.length -1; j--) {
+			}
+		
+			
+		}
+		
+	}
+	
+
